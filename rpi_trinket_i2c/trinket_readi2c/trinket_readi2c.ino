e8190271e1517773b8d9aa97a6c794bb2c0746b3
giraffe <evil_giraffe_319@hotmail.com>
2014-10-23 21:59:32 +0100

    Changed trinket to flash Red/Green in response to I2C signals

--- rpi_trinket_i2c/trinket_readi2c/trinket_readi2c.ino
+++ rpi_trinket_i2c/trinket_readi2c/trinket_readi2c.ino
@@ -1,23 +1,31 @@
 #include <TinyWireS.h>
 
 int i2cAddress = 0x3b;
-int pinLed = 1;
+int pinR = 1;
+int pinG = 3;
+int pinB = 4;
 
 void setup() {
-  pinMode(pinLed, OUTPUT);
+  pinMode(pinR, OUTPUT);
+  pinMode(pinG, OUTPUT);
+  pinMode(pinB, OUTPUT);
   TinyWireS.begin(i2cAddress);
 }
 
 void loop() {
-  while(0 > TinyWireS.available()) {
+  while(0 < TinyWireS.available()) {
     int value = TinyWireS.receive();
     
     if (1 == value) {
-      digitalWrite(pinLed, HIGH);
+      digitalWrite(pinR, LOW);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinB, LOW);
     }
     
     if (0 == value) {
-      digitalWrite(pinLed, LOW);
+      digitalWrite(pinR, HIGH);
+      digitalWrite(pinG, LOW);
+      digitalWrite(pinB, LOW);
     }
   }
 }
