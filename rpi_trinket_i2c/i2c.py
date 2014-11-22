e119a0e09d6dd876ec6dcd91d6f5037b08f70bd9
giraffe <evil_giraffe_319@hotmail.com>
2014-11-22 21:26:47 +0000

    Trinket uses NeoPixel to indicate status - throbbing blue for unknown, constant green for build OK, spinning red for build failed. Pi checks TeamCity for build status

--- rpi_trinket_i2c/i2c.py
+++ rpi_trinket_i2c/i2c.py
@@ -1,10 +1,18 @@
 import smbus
 import time
+
+import httplib2
+h = httplib2.Http(".cache")
+
 bus = smbus.SMBus(1)
 address = 0x3b
 
+h.add_credentials('admin', 'password')
+
 while True:
-    bus.write_byte_data(address, 0, 1)
-    time.sleep(0.5)
-    bus.write_byte_data(address, 0, 0)
-    time.sleep(0.5)
+    (resp_headers, status) = h.request("http://192.168.0.5:91/httpAuth/app/rest/builds/buildType:TestProject_TestBuild/status", "GET")
+    if status == "SUCCESS":
+        bus.write_byte_data(address, 0, 1)
+    else:
+        bus.write_byte_data(address, 0, 0)
+    time.sleep(5)
--- rpi_trinket_i2c/trinket_readi2c/trinket_readi2c.ino
+++ rpi_trinket_i2c/trinket_readi2c/trinket_readi2c.ino
@@ -1,31 +1,105 @@
+#include <Adafruit_NeoPixel.h>
 #include <TinyWireS.h>
 
+
 int i2cAddress = 0x3b;
-int pinR = 1;
-int pinG = 3;
-int pinB = 4;
+
+int pin = 1;
+int numPixels = 12;
+
+int currentStatus = -1;
+
+int animationState = 0;
+int wait = 50;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, pin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(pinR, OUTPUT);
-  pinMode(pinG, OUTPUT);
-  pinMode(pinB, OUTPUT);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
   TinyWireS.begin(i2cAddress);
 }
 
 void loop() {
+  int oldStatus = currentStatus;
+
+  // Read the bus
   while(0 < TinyWireS.available()) {
+    // update currentStatus if we have a new value
     int value = TinyWireS.receive();
-    
-    if (1 == value) {
-      digitalWrite(pinR, LOW);
-      digitalWrite(pinG, HIGH);
-      digitalWrite(pinB, LOW);
+    currentStatus = value;
+  }
+
+  switch(currentStatus) {
+  case 1:
+    // Build is good
+    // NeoPixel shows constant green (only need to set it on entering this state)
+    if (currentStatus != oldStatus)
+    {
+      setConstantColor(strip.Color(0,255,0));
+    }
+    break;
+  case 0:
+    // Build is broken
+    // NeoPixel chases red
+    if (currentStatus != oldStatus) {
+      animationState = 0;
+    }
+    if (numPixels - 1 < animationState) {
+      animationState = 0;
     }
     
-    if (0 == value) {
-      digitalWrite(pinR, HIGH);
-      digitalWrite(pinG, LOW);
-      digitalWrite(pinB, LOW);
+    setChasingRed(animationState);
+    animationState++;
+    break;
+  default:
+    // Build is unknown
+    // NeoPixel throbs blue
+    if (currentStatus != oldStatus) {
+      animationState = 0;
     }
+    if (31 < animationState) {
+      animationState = 0; 
+    }
+    if (16 > animationState) {
+      setConstantColor(strip.Color(0, 0, animationState * 16));
+    }
+    else {
+      setConstantColor(strip.Color(0, 0, (31 - animationState) * 16));
+    }
+    animationState++;
+    break;
+  }
+
+  delay(wait);
+}
+
+void setConstantColor(uint32_t color) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+  }
+  strip.show();
+}
+
+void setChasingRed(int pixel) {
+  for(int i=0; i< strip.numPixels(); i++) {
+    if (i == pixel) {
+      strip.setPixelColor(i, strip.Color(255, 0, 0));
+    }
+    else {
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
+    }
+    strip.show();
   }
 }
+
+
+
